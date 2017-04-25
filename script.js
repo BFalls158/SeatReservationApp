48b3a72e2b4526f5b1f916256ba3804af47d32b1
Bryan Falls <bfalls1587@gmail.com>
2017-04-25 16:51:53 -0400

    todo

---
+++ script.js
@@ -0,0 +1,5 @@
+//	 TODO Build object for each sumbited reservation
+//	 TODO Clicking needs to have toggle for class & populate an array of currently clicked
+//	 TODO Submitting form needs change selected to reserved
+// 	 TODO On hover should make info appear on reserved seats
+//	 TODO 
