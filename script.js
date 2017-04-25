5d626be4816d392e30fc6ce337db63d0e2c36d71
Bryan Falls <bfalls1587@gmail.com>
2017-04-25 19:22:36 -0400

    obj constructor for new seat upon submit

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-//	 TODO Build object for each sumbited reservation
+$(document).ready(function(){
+//Build object for each sumbited reservation
+// added .reserved in case we need it to determine if a seat is taken
+var Seat = function(first, last, email, phone, loc) {
+	this.firstName = first;
+	this.lastName = last;
+	this.email = email;
+	this.phone = phone;
+	this.location = loc;
+	this.reserved = true;
+}
 //	 TODO Clicking needs to have toggle for class & populate an array of currently clicked
 //	 TODO Submitting form needs change selected to reserved
 // 	 TODO On hover should make info appear on reserved seats
 //	 TODO 
 
-console.log($('.seat'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
